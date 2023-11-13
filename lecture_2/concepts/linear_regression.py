14842f0d78eb8d6f7e58bf9705d1ca62c6ed3d14
GellertPalfi <palfigell9@gmail.com>
2023-11-13 16:09:51 +0000

    updated lin reg and tests

--- lecture_2/concepts/linear_regression.py
+++ lecture_2/concepts/linear_regression.py
@@ -1,73 +1,124 @@
 import numpy as np
+from numpy.typing import ArrayLike
 from concepts.mean_squared_error import mean_squared_error
 from typing import Tuple
 
+from sklearn.linear_model import LinearRegression
 
-def lin_reg(iterations: int, learning_rate: float, m: float, b: float, X, y):
-    """Calculate optimal linear regression parameters using batch gradient descent.
+class LinReg:
+    def __init__(self) -> None:
+        self.intercept = 0
+        self.coefs = []
+        self.loss = []
 
-    This is a really primitive implementation of linear regression and
-    it is not optimized and only works for 1D arrays.
+    def fit(self, iterations: int, learning_rate: float, X: ArrayLike, y: ArrayLike) -> tuple[list, list]:
+        """Calculate optimal linear regression parameters using batch gradient descent.
 
-    Args:
-        iterations: Number of iterations to train the model for.
-        learning_rate: The step size for each iteration of gradient descent.
-        m: The initial value of the slope of the regression line.
-        b: The initial value of the intercept of the regression line.
-        X (array-like): The input feature values.
-        y (array-like): The true output values corresponding to each input.
-            Should be same length as X.
+        This is a really primitive implementation of linear regression and
+        early stopping.
 
-    Returns:
-        steps: List containing m and b in tuples for every iteration.
-        loss: List containing MSE for every iteration.
-    """
+        Args:
+            iterations: Number of iterations to train the model for.
+            learning_rate: The step size for each iteration of gradient descent.
+            X (array-like): The input feature values.
+            y (array-like): The true output values corresponding to each input.
+                Should be same length as X.
 
-    # every element of steps except the last one and loss are only
-    # for visualization purposes
-    # the last tuple in steps contained the final values for m and b
-    steps = []
-    loss = []
+        Returns:
+            steps: List containing m and b in tuples for every iteration.
+            loss: List containing MSE for every iteration.
+        """
+        # initalize 0 coeffs and intercept
+        # add fake columns of 1 for better intercept fit
 
-    for _ in range(iterations):
-        y_predicted = np.array([m * X]) + b
-        grad_m: float
-        grad_b: float
-        grad_m, grad_b = compute_gradients(X, y, y_predicted)
-        m -= learning_rate * grad_m
-        b -= learning_rate * grad_b
-        steps.append((m, b))
+        theta = np.random.rand(X.shape[1] + 1)*10
+        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        steps = []
+        self.loss = []
 
-        mse: float = mean_squared_error(y, y_predicted)
-        loss.append(mse)
+        for _ in range(iterations):
+            # predict y with current coeffs
+            y_predicted = np.dot(X, theta)
+            # calculate and update by gradient
+            gradients = self._compute_gradients(X, y, y_predicted)
+            theta -= learning_rate * gradients
+            print(theta)
+            mse = mean_squared_error(y, y_predicted)
 
-    return steps, loss
+            # primtive early stopping
+            if np.isnan(mse) or np.isinf(mse):
+                break
 
-
-def compute_gradients(X, y_actual, y_predicted) -> Tuple[float]:
-    """Calculate gradient for MSE."""
-    error: float = y_actual - y_predicted
-    grad_m: float = np.mean(-2 * (X * error))  # Gradient for the slope (m)
-    grad_b: float = np.mean(-2 * error)  # Gradient for the intercept (b)
-    return (grad_m, grad_b)
+            # log metrics
+            steps.append(np.copy(theta))
+            self.loss.append(mse)
 
 
-def predict(x_new, m, b):
-    """Predict new values with the trained linear regression model."""
-    return m * x_new + b
+        self.intercept = steps[-1][0]
+        self.coefs = steps[-1][1:]
+        return steps
+
+
+    def _compute_gradients(self, X, y_actual, y_predicted) -> Tuple[float]:
+        """Calculate gradient for MSE."""
+        errors = y_actual - y_predicted
+        gradients = -2 * np.mean(X * errors[:, np.newaxis], axis=0)
+        return gradients
+
+
+
+    def predict(self, x_new: ArrayLike):
+        """Predict new values with the trained linear regression model."""
+        x_new = np.array(x_new)
+
+        if x_new.ndim == 1:
+            return np.dot(self.coefs, x_new) + self.intercept
+        else:
+            return np.dot(x_new, self.coefs) + self.intercept
+         
+
 
 
 if __name__ == "__main__":
-    # example usage
-    x = np.array([0, 1, 2, 3, 4, 5])
-    y = np.array([1, 3, 5, 8, 10, 13])
-    initial_m = 0
-    initial_b = 0
-    iterations = 50
+    iterations = 1000
     learning_rate = 0.01
 
-    steps, loss = lin_reg(iterations, learning_rate, initial_m, initial_b, x, y)
-    final_m, final_b = steps[-1]
-    new_x = 6
-    predicted = predict(6, final_m, final_b)
-    print(predicted)  # 15.061807807592944
+    # example usage 1D
+    y = np.array([1, 3, 5, 8, 10, 13])
+    x = np.array([1, 2, 6, 10, 11, 14]).reshape(-1,1)
+    
+    own_reg = LinReg()
+    own_reg.fit(iterations, learning_rate, x, y)
+
+    reg = LinearRegression()
+    reg.fit(x,y)
+    
+    print(own_reg.coefs, own_reg.intercept) # [0.5] 1.8763348981580675e-09
+    print(reg.coef_, reg.intercept_) # [0.5] -1.7763568394002505e-15
+    raise
+    new_x = np.ndarray([6]).reshape(-1,1)
+    print(reg.predict(new_x)) # [-2.83333333 -1.83333333 -0.83333333  0.66666667  1.66666667  3.16666667]
+    print(own_reg.predict(new_x)) # [-2.83333333 -1.83333333 -0.83333333  0.66666667  1.66666667  3.16666667]
+
+    # example usage 2D
+    x = np.array([[ 0,  0],
+       [ 1,  1],
+       [ 2,  4],
+       [ 3,  9],
+       [ 4, 16],
+       [ 5, 25]])
+    
+    
+    own_reg = LinReg()
+    own_reg.fit(iterations, learning_rate, x, y)
+
+    reg= LinearRegression()
+    reg.fit(x,y)
+    
+    print(own_reg.coefs, own_reg.intercept) # [1.95327313 0.08933628] 0.9646065741404198
+    print(reg.coef_, reg.intercept_) # [1.95357143 0.08928571] 0.9642857142857162
+
+
+    new_x = np.ndarray([6,2])
+    print(reg.predict(new_x)) # [-4.73809524 -2.6952381  -0.47380952  1.92619048  4.5047619   7.26190476]
+    print(own_reg.predict(new_x)) # [-4.73749218 -2.69488276 -0.47360078  1.92635376  4.50498087  7.26228054]
--- lecture_2/visualize/vis_reg_fit.py
+++ lecture_2/visualize/vis_reg_fit.py
@@ -1,4 +1,4 @@
-from concepts.linear_regression import lin_reg
+from concepts.linear_regression import LinReg
 from matplotlib.animation import FuncAnimation
 
 import numpy as np
@@ -6,16 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.array([0, 1, 2, 3, 4, 5, 7, 9])
-y = np.array([1, 3, 5, 8, 10, 13, 17, 23])
-m = np.random.randint(0, 10)
-b = np.random.randint(0, 10)
-learning_rate = 0.03
-max_iters = 100
-
-slopes, loss = lin_reg(max_iters, learning_rate, m, b, x, y)
-
-
+y = np.array([1, 3, 5, 8, 10, 13])
+x = np.array([1, 2, 6, 10, 11, 14]).reshape(-1,1)
+max_iters = 1000
+learning_rate = 0.01
+reg = LinReg()
+slopes = reg.fit(max_iters, learning_rate,x,y )
+print(slopes)
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 
 ax1.set_xlim(0, max(x) + 1)
@@ -24,7 +21,7 @@ data = ax1.scatter(x=x, y=y)
 (line1,) = ax1.plot([], [], "b", lw=2)
 
 ax2.set_xlim(0, max_iters)
-ax2.set_ylim(0, max(loss))
+ax2.set_ylim(0, max(reg.loss))
 ax2.set_xlabel("Iterations")
 ax2.set_ylabel("MSE")
 ax2.legend(["Loss"], loc="upper right")
@@ -35,18 +32,21 @@ ax2.legend(["Loss"], loc="upper right")
     lw=2,
 )
 
-
+iter = 0
 def update(frame):
+    global iter
+    print(f"{iter}/{max_iters}")
+    iter += 1
     # Update data for the regression line plot
-    m, b = slopes[frame]
-    x_values = np.linspace(0, max(x), 100)
+    b, m = slopes[frame]
+    x_values = np.linspace(0, 100, 200)
     y_values = m * x_values + b
     line1.set_data(x_values, y_values)
     ax1.legend([data, line1], ["Data points", "Regression Line"], loc="lower right")
     ax2.legend(["Loss"], loc="upper right")
 
     # Update data for the loss plot
-    line2.set_data(range(frame + 1), loss[: frame + 1])
+    line2.set_data(range(frame + 1), reg.loss[: frame + 1])
 
     # Return both line objects
     return line1, line2
@@ -55,4 +55,4 @@ def update(frame):
 ani = FuncAnimation(fig, update, frames=max_iters, interval=500, blit=True)
 
 # Save the animation
-ani.save("linear_regression_progress.gif", writer="imagemagick", fps=4)
+ani.save("linear_regression_progress.gif", writer="pillow", fps=4)
